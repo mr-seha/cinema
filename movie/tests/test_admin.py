46d71a0b94eae95f30aa82622d6d407527ae6c7f
Mohammadreza <mohammadreza.souri.07@gmail.com>
2025-05-06 10:39:25 +0330

    small changes in admin tests in movie app

--- movie/tests/test_admin.py
+++ movie/tests/test_admin.py
@@ -9,14 +9,13 @@ ADMIN_FILM_URL = reverse("admin:movie_film_changelist")
 @pytest.mark.django_db
 class TestAdmin:
     def test_film_list(self, api_client, authenticate):
-        admin_user_payload = dict(
+        payload = dict(
             username="admin",
             email="admin@gmail.com",
             password="admin_password",
-            is_staff=True,
         )
 
-        user = get_user_model().objects.create_superuser(**admin_user_payload)
+        user = get_user_model().objects.create_superuser(**payload)
         api_client.force_login(user=user)
 
         response = api_client.get(ADMIN_FILM_URL)
