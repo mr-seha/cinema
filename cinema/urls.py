5c88a5cdf279bd2c7f3b8b7e381d7fba10cbd3bf
Mohammadreza <mohammadreza.souri.07@gmail.com>
2025-05-29 09:24:50 +0330

    Fix gunicorn not serving static files

--- cinema/urls.py
+++ cinema/urls.py
@@ -2,6 +2,7 @@ from debug_toolbar.toolbar import debug_toolbar_urls
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path, include
 from drf_spectacular.views import (
     SpectacularAPIView,
@@ -42,6 +43,8 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns()
+
     urlpatterns += static(
         settings.MEDIA_URL,
         document_root=settings.MEDIA_ROOT
